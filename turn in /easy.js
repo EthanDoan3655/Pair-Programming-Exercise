5f50ee3d53a0c00de0c8bd2a1f1cc90c0a0600b8
YoanaAriasMagana <102987897+YoanaAriasMagana@users.noreply.github.com>
2022-05-04 13:37:41 -0400

    Update easy.js
    Fixed easy 2

--- turn in /easy.js
+++ turn in /easy.js
@@ -25,24 +25,23 @@ console.log(splitOddAndEven([2, 4, 7, 11, 15, 16]))
 
 //easy 2: yoana solved
 
-/*Create a function that accepts two strings, then determines whether or not the first string is an anagram of the
-second string by returning a boolean.
-Example:
-Input: String 1: So dark the con of man
-String 2: Madonna of the Rocks
-Output: True
-Input: String 1: Things are good
-String 2: Dogs eat ants
-Output: False
+/*Create a function that checks an array for prime numbers then inserts any pimes into a new array.
+Example 1:
+Input: numArray = [1,2,3,4,5,6,7,8,9,10]
+Output: primeArray = [2,3,5,7]
+Example 2:
+Input: numArray = [10, 18, 19, 29, 33, 35, 47, 66, 83]
+Output: primeArray = [19 ,29 ,47 , 83]
 */
-function Compare(str1, str2) {
-    if (str1.length !== str2.length) {
-        return false
-    }
+let numberArray = [10, 18, 19, 29, 33, 35, 47, 66, 83]
 
-    return str1.toLowerCase().split("").sort().join("") === str2.toLowerCase().split("").sort().join("")
+function isPrime(num) {
+    for (let i = 2; num > i; i++) {
+        if (num % i == 0) {
+            return false;
+        }
+    }
+    return num > 1;
 }
 
-console.log(Compare("SoDarkTheConOfMan", "MadonnaOfTheRocks"))
-console.log(Compare("Things are good", "Dogs eat ants"))
-//couldn't figure out how to make the spaces not count as characters
+console.log(numberArray.filter(isPrime));
